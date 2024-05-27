8ff5a3ca63492883a745e9345b6cef0db59b0d2d
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-26 23:40:04 -0700

    check for ads

--- scripts/content.js
+++ scripts/content.js
@@ -20,10 +20,9 @@ if (!checkForVideoElement()) {
 function checkForVideoElement() {
     var video = document.querySelector("video");
     if (video) {
-        console.log(video);
+        console.log("video found");
 
-        if (video.currentTime <= 60) {
-            console.log("Video found less than 60s");
+        if (isMainVideo && isVideoBeginning(video)) {
             // asking background.js to create an offscreen document and play the sound
             chrome.runtime.sendMessage("runOffscreenTask");
         }
@@ -31,4 +30,14 @@ function checkForVideoElement() {
         return true;
     }
     return false;
+}
+
+// checks if the video is at the beginning
+function isVideoBeginning(video) {
+    return video.currentTime <= 2
+}
+
+// checks if the video is the main video or an ad (<30s)
+function isMainVideo(video) {
+    return video.duration > 30
 }
